e635cefc9cfe83882b4603068488eaf0a12e24eb
Geert Custers <geert.aj.custers@gmail.com>
2017-05-25 10:01:54 +0200

    Finish verify_ssh_host function
    One thing that needs to be considered however, is dealing with a
    situation in which the host is not known. This isn't yet handled by the
    function, it just simply returns BACKUPSSH_HOST_UNKNOWN.

--- include/ssh.h
+++ include/ssh.h
@@ -5,16 +5,19 @@
 #include <error.h>
 
 /* creates an ssh session from the arguments */
-ssh_session create_ssh_session(ssh_error *error, const char *host, int verbosity, int port);
+ssh_session create_ssh_session(int *err, const char *host, int verbosity, int port);
 
 /* verifies if the ssh host is known */
-ssh_error verify_ssh_host(ssh_session session);
+int verify_ssh_host(ssh_session session);
+
+/* checks if the host is known to us */
+int is_host_known(char *hash);
 
 /* authenticates the host with the given method */
-ssh_error authenticate_ssh_host(ssh_session session, int method,
+int authenticate_ssh_host(ssh_session session, int method,
 	       	const char *password, const char *key_file);
 
 /* starts the rsync signature retrieval process */
-ssh_error start_rsync_sig(ssh_session session, const char* dir);
+int start_rsync_sig(ssh_session session, const char* dir);
 
 #endif
--- src/ssh.c
+++ src/ssh.c
@@ -1,7 +1,9 @@
 #include <ssh.h>
 #include <error.h>
+#include <stdio.h>
+#include <string.h>
 
-ssh_session create_ssh_session(ssh_error *error, const char *host, int verbosity, int port)
+ssh_session create_ssh_session(int *err, const char *host, int verbosity, int port)
 {
 	ssh_session session = ssh_new();
 	if(session == NULL)
@@ -10,19 +12,50 @@ ssh_session create_ssh_session(ssh_error *error, const char *host, int verbosity
 	ssh_options_set(session, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
 	ssh_options_set(session, SSH_OPTIONS_PORT, &port);
 
-	*error = SSH_SUCCESS;
+	*err = BACKUPSSH_SUCCESS;
 	return session;
 }
 
-ssh_error verify_ssh_host(ssh_session session)
+int verify_ssh_host(ssh_session session)
 {
-	int hlen;
+	int err;
+	ssh_key key;
+	size_t hlen;
 	unsigned char *hash = NULL;
 	char *hexa;
-	char buf[10];
 
-	hlen = ssh_get_pubkey_hash(session, &hash);
-	if(hlen < 0)
-		return SSH_LENGTH_ERR;
-	return SSH_SUCCESS;
+	/* get the public key */
+	err = ssh_get_publickey(session, &key);
+	if(err == SSH_ERROR)
+		return BACKUPSSH_PUBKEY_ERR;
+
+	/*  get the hash */
+	err = ssh_get_publickey_hash(key, SSH_PUBLICKEY_HASH_SHA1, &hash,
+			&hlen);
+	if (err == -1)
+		return BACKUPSSH_HASH_ERR;
+	/*  get the cool hash */
+	hexa = ssh_get_hexa(hash, hlen);
+	if(hexa == NULL)
+		return BACKUPSSH_HASH_ERR;
+	/*  leave the actual work to another function */
+	return is_host_known(hexa);
+}
+
+int is_host_known(char *hash)
+{
+	/*  open known hosts file */
+	FILE *fp = fopen("hosts.kwn", "r");
+	if(fp == NULL)
+		return BACKUPSSH_HOST_UNKNOWN;
+
+	char buf[4096];
+	do {
+		fseek(fp, 1, SEEK_CUR);
+		fgets(&buf[0], 4096, fp);
+		if(strcmp(&buf[0], hash) == 0)
+		return BACKUPSSH_HOST_KNOWN;
+	} while(fgetc(fp) != EOF);
+
+	return BACKUPSSH_HOST_UNKNOWN;
 }
